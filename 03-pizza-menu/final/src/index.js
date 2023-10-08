9447ac2c489985fc1b1cf37f9403b4c8fa17e9b2
tachyonok <tachyonparticle123@gmail.com>
2023-10-08 10:38:21 +0530

    new

--- 03-pizza-menu/final/src/index.js
+++ 03-pizza-menu/final/src/index.js
@@ -2,8 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 function App() {
-  return <h1>Hello React</h1>;
+  return (
+    <div>
+      <h1>Hello React</h1>
+      <Pizza />
+    </div>
+  );
 }
+
+// function name must start with uppercase
+function Pizza() {
+  return <h2>Pizza</h2>;
+}
+
 //React v18
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
